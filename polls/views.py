b632c6217a97625fa430ff2442233c32ce49bce3
Christopher Gonzalez <chris.gonzalez9388@gmail.com>
2026-09-08 15:03:03 -0700

    EXP: Stoped at HttpResponseRedirect step

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
-from .models import Question
+from .models import Question, Choice
 
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -23,4 +24,19 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            }
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+
+        return HttpResponseRedirect
