d62d20443de980531f2d4033a6eca4d6275feae1
Christopher Gonzalez <chris.gonzalez9388@gmail.com>
2026-09-08 13:25:34 -0700

    Changed detail method to include shortcuts and removed Http404 import

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 
 from .models import Question
 
-from django.http import Http404
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -14,10 +13,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, "polls/detail.html", {"question": question})
 
 
