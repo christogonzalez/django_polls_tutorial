a9d0def3dc17fd2e2007818c17a06a13d741476d
Christopher Gonzalez <chris.gonzalez9388@gmail.com>
2026-09-09 09:34:48 -0700

    Adding voting logic and view to support additional votes

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,5 @@
+from django.db.models import F
+
 from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse, HttpResponseRedirect
@@ -5,6 +7,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 
 from django.urls import reverse
+
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -19,24 +22,25 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of questions %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
+        # .POST[] is dictionary-like object that returns string
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
+    except KeyError, Choice.DoesNotExist:
         return render(
             "polls/detail.html",
             {
                 "question": question,
                 "error_message": "You didn't select a choice.",
-            }
+            },
         )
     else:
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
 
-        return HttpResponseRedirect
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
