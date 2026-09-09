0b0e3b3bafe993f3595fa64a2f7ab38de1795503
Christopher Gonzalez <chris.gonzalez9388@gmail.com>
2026-09-09 15:55:00 -0700

    Added css styles to polls index and started personalizing  admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # Determines order that fields appear on admin side
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
