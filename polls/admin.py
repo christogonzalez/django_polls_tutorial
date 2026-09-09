971b86fdf652575b6acca1042c10c4cbd1150ab5
Christopher Gonzalez <chris.gonzalez9388@gmail.com>
2026-09-09 16:12:56 -0700

    Compacted and added more info in both question list view and detail view on Admin side

--- polls/admin.py
+++ polls/admin.py
@@ -3,17 +3,20 @@ from django.contrib import admin
 from .models import Choice, Question
 
 
-class ChoiceInLine(admin.StackedInline):
+class ChoiceInLine(admin.TabularInline):
     model = Choice
     extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
     # Determines order that fields appear on admin side
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]}),
     ]
+
     inlines = [ChoiceInLine]
 
 
